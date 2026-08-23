6f2e83e1b78590ac33a228691e187a43d9a3a9a9
Keiren-b <keiren_b@hotmail.com>
2026-08-23 13:30:43 +1000

    inital data clean issues

--- 00_eda.sql
+++ 00_eda.sql
@@ -9,3 +9,4 @@ all_varchar = true);
 -- Data is scattered across in long format, with years spread out as 4 columns
 -- Column headers are inconsistently named
 -- 2018: Multiple rows per day, 2 different tours a day, multiple guides with a binary yes/no if they're needed. Number of people per guide not total
+
