94779c566e13b474e3133a7790c258ded8c6a33e
Keiren-b <keiren_b@hotmail.com>
2026-08-23 13:41:43 +1000

    2019 initial cleaning

--- 00_eda.sql
+++ 00_eda.sql
@@ -35,16 +35,34 @@ FROM read_xlsx(
 WHERE B IS NOT NULL;
 
 
-SELECT * FROM year_2018
--- Any rows that failed to convert?
---SELECT count(*) AS total, count(tour_date) AS parsed FROM year_2018;
+SELECT * FROM year_2018;
 
--- Do the dates fall in the year you expect?
---SELECT min(tour_date), max(tour_date) FROM year_2018;
+-------------------
+-- 2019 Cleaning
+------------------
+-- Columns now include a start and end headcount figure. End likely to be dropped in analysis
 
+CREATE OR REPLACE TABLE year_2019 AS
+SELECT
+    DATE '1899-12-30' + TRY_CAST(I AS INTEGER) AS tour_date,
+    CAST(J AS TEXT) AS tour_type,
+    CAST(K AS TEXT) AS guide,
+    CASE lower(trim(L))
+        WHEN 'yes' THEN TRUE
+        WHEN 'no'  THEN FALSE
+    END AS needed,
+    TRY_CAST(M AS INTEGER) AS headcount_per_group_start,
+    TRY_CAST(N AS INTEGER) AS headcount_per_group_end
+FROM read_xlsx(
+    'data/raw/numbers_raw.xlsx',
+    header      = false,
+    sheet       = 'SS Info dump',
+    range       = 'I3:N',
+    all_varchar = true
+)
+WHERE I IS NOT NULL;
 
-
-
+SELECT * from year_2019
 --CREATE or REPLACE TABLE year_2019 AS
 --SELECT * FROM read_xlsx(
 --    'data/raw/numbers_raw.xlsx', 
