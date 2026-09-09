4c21823107a37200e8d7df939bd143e4605e1d4e
Keiren-b <keiren_b@hotmail.com>
2026-09-09 19:57:48 +1000

    initial configs

--- tours/config.py
+++ tours/config.py
@@ -2,23 +2,29 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 from loguru import logger
+import datetime as dt
+from dataclasses import dataclass
+from pathlib import Path
 
 # Load environment variables from .env file if it exists
 load_dotenv()
 
 # Paths
-PROJ_ROOT = Path(__file__).resolve().parents[1]
-logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+logger.info(f"PROJ_ROOT path is: {PROJECT_ROOT}")
 
-DATA_DIR = PROJ_ROOT / "data"
+# FEATURE_TABLE = PROJECT_ROOT / "data" / "processed" / "features.parquet"
+RESULTS_DIR = PROJECT_ROOT / "results"
+
+DATA_DIR = PROJECT_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
 INTERIM_DATA_DIR = DATA_DIR / "interim"
 PROCESSED_DATA_DIR = DATA_DIR / "processed"
 EXTERNAL_DATA_DIR = DATA_DIR / "external"
 
-MODELS_DIR = PROJ_ROOT / "models"
+MODELS_DIR = PROJECT_ROOT / "models"
 
-REPORTS_DIR = PROJ_ROOT / "reports"
+REPORTS_DIR = PROJECT_ROOT / "reports"
 FIGURES_DIR = REPORTS_DIR / "figures"
 
 # If tqdm is installed, configure loguru with tqdm.write
